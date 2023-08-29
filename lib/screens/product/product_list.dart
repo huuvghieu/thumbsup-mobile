edbdfea691cde219498762a77f67134fa95c897c
@thinhphamquoc9999 <thinhphamquoc9999@gmail.com>
2023-08-29 17:37:57 +0700

    Update store_carousel.dart

--- lib/screens/product/product_list.dart
+++ lib/screens/product/product_list.dart
@@ -3,26 +3,52 @@ import 'package:flutter/material.dart';
 import '../../common/color.dart';
 
 class ProductList extends StatefulWidget {
-  const ProductList({super.key});
+  const ProductList({Key? key}) : super(key: key);
 
   @override
   State<ProductList> createState() => _ProductListState();
 }
 
 class _ProductListState extends State<ProductList> {
+  final List<Map> myProducts =
+      List.generate(20, (index) => {"id": index, "name": "Product $index"})
+          .toList();
+
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       children: [
-        Align(
+        const Align(
           alignment: AlignmentDirectional.topStart,
           child: Text(
-                "Sản phẩm nổi bật",
-                style: TextStyle(
-                  color: AppColor.lowText,
-                  fontSize: 22.0,
-                ),
+            "Sản phẩm nổi bật",
+            style: TextStyle(
+              color: AppColor.lowText,
+              fontSize: 22.0,
+            ),
+          ),
+        ),
+        const SizedBox(height: 10.0),
+        GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10.0,
+            crossAxisSpacing: 10.0,
+            childAspectRatio: 1,
+          ),
+          itemCount: myProducts.length,
+          itemBuilder: (BuildContext ctx, index) {
+            return Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(15),
               ),
+              child: Text(myProducts[index]["name"]),
+            );
+          },
         ),
       ],
     );
